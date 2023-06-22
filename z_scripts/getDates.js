140087c8b508e4e86598da51fd052221fe163e33
MagnesiumSail <Leapster16@gmail.com>
2023-06-22 08:49:08 -0600

    Finished Homepage Responsiveness

--- z_scripts/getDates.js
+++ z_scripts/getDates.js
@@ -55,3 +55,15 @@ modeButton.addEventListener("click", () => {
     }
 });
 
+const visitSelect = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitSelect.textContent = numVisits
+} else {
+    visitSelect.textContent = 'This is your first visit. Welcome!';
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls" , numVisits);
