02a0ba3fb487c24df1ca54997399f50e6172b20e
MagnesiumSail <Leapster16@gmail.com>
2023-07-14 15:23:42 -0600

    added dynamic links

--- z_scripts/links.js
+++ z_scripts/links.js
@@ -3,18 +3,52 @@ const linksURL = "https://magnesiumsail.github.io/wdd230/data/links.json";
 
 async function getLinksData() {
     try {
-        const response = await(linksURL);
-        if (response.ok)
-        {
-            const data = response.json
-            console.log(data);
-        }
-        else
-        {
-            throw new Error("Failed to fetch links json")
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            displayLinks(data.weeks);
+        } else {
+            throw new Error("Failed to fetch links json");
         }
     } catch (error) {
-        console.log("Error: " , error.message);
+        console.log("Error: ", error.message);
     }
 }
+
+
+function displayLinks(weeks) {
+    const ul = document.querySelector(".MIDDLE #l3ft ul");
+
+    ul.innerHTML = "";
+
+    console.log('Weeks data:', weeks);
+
+    weeks.forEach(weekData => {
+        console.log('Week data:', weekData);
+
+        const li = document.createElement("li");
+        li.textContent = `${weekData.week}: `;
+        
+        weekData.links.forEach((link, index) => {
+            console.log('Link data:', link);
+
+            const a = document.createElement("a");
+            a.href = baseURL + link.url;
+            a.textContent = link.title;
+
+            li.appendChild(a);
+
+            if (index !== weekData.links.length - 1) {
+                li.append(' | ');
+            }
+        });
+        
+        ul.appendChild(li);
+    });
+}
+
+
+
+
+
 getLinksData();
