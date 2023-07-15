219f0478e21981757f4d18cb259fe2b4f3ae84bd
MagnesiumSail <Leapster16@gmail.com>
2023-07-14 21:38:38 -0600

    finished directory page.

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,25 +1,49 @@
-window.onload = function() {
-    fetch('https://github.com/MagnesiumSail/wdd230/blob/main/data/members.json')  // replace with your JSON URL
-        .then(response => response.json())
-        .then(data => {
-            let mainSection = document.getElementById('mainSection');
+let mainSection = document.getElementById('mainSection');
+let toggleButton = document.getElementById('toggleButton');
 
-            data.companies.forEach(company => {
-                let card = document.createElement('div');
-                card.className = 'card';
-                card.innerHTML = `
-                    <img src="${company.image}" alt="${company.name}">
-                    <div class="container">
-                        <h2>${company.name}</h2>
-                        <p><b>Address:</b> ${company.address}</p>
-                        <p><b>Phone:</b> ${company.phone}</p>
-                        <p><b>Website:</b> <a href="${company.website}">${company.website}</a></p>
-                        <p><b>Membership Level:</b> ${company.membershipLevel}</p>
-                        <p><b>Additional Info:</b> ${company.additionalInfo}</p>
-                    </div>
-                `;
-                mainSection.appendChild(card);
-            });
-        })
-        .catch(error => console.error('Error:', error));
-};
+fetchData('https://magnesiumsail.github.io/wdd230/data/members.json')
+    .then(data => createCards(data, mainSection));
+
+toggleButton.addEventListener('click', function() {
+    toggleView(mainSection);
+});
+
+
+async function fetchData(url) {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function createCard(company) {
+    let card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
+        <img src="${company.image}" alt="${company.name}">
+        <div class="container">
+            <h2>${company.name}</h2>
+            <p><b>Address:</b> ${company.address}</p>
+            <p><b>Phone:</b> ${company.phone}</p>
+            <p><b>Website:</b> <a href="${company.website}">${company.website}</a></p>
+            <p><b>Membership Level:</b> ${company.membershipLevel}</p>
+            <p><b>Additional Info:</b> ${company.additionalInfo}</p>
+        </div>
+    `;
+    return card;
+}
+
+function createCards(data, containerElement) {
+    data.companies.forEach(company => {
+        let card = createCard(company);
+        containerElement.appendChild(card);
+    });
+}
+
+function toggleView(element) {
+    element.classList.toggle('grid-view');
+}
+
